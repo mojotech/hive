a91b1400060a0d615a52a9719df42d787b77de62
Joel Hawksley <joel@mojotech.com>
2015-03-13 14:07:56 -0400

    [AUTH] Add github authenticated sessions.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env["omniauth.auth"]
+
+    user = User.find_by(github_user_id: auth["uid"]) || create_user(auth)
+
+    session[:user_id] = user.id
+
+    redirect_to root_url, notice: 'Signed in!'
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Signed out!'
+  end
+
+  private
+
+  def create_user(auth)
+     User.create(github_user_id: auth["uid"],
+                 name: auth["info"]["name"],
+                 email:  auth["info"]["email"],
+                 auth_token: auth["credentials"]["token"])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :sign_out
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
