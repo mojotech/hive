f9ff669fdc3ab45389aa7ef9d2fbe91761c28049
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-03-20 12:58:34 -0400

    List a user's repositories on their landing page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      repositories = current_user.repositories
+      repository_names = repositories.map(&:full_name)
+      render locals: { repository_names: repository_names }
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :auth_token, presence: true
+
+  def repositories
+    user_repositories = github_client.repositories
+    organization_repositories = github_client.organizations.reduce([]) do |repositories, organization|
+      repositories + github_client.organization_repositories(organization.login, type: 'member')
+    end
+    user_repositories + organization_repositories
+  end
+
+  private
+
+  def github_client
+    @github_client ||= Octokit::Client.new(access_token: auth_token, auto_paginate: true)
+  end
 end
