6bef3227ff1a64c331ab4fca4a8e68034ffe377d
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-03-20 12:58:34 -0400

    List a user's repositories on their landing page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   validates :nickname, presence: true
   validates :email, presence: true
   validates :auth_token, presence: true
+
+  def organizations
+    github_client.organizations
+  end
+
+  private
+
+  def github_client
+    @github_client ||= Octokit::Client.new(access_token: auth_token, auto_paginate: true)
+  end
 end
