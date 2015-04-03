c321c5e143d3ba25565910edc091b47d24f2d397
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-04-03 15:57:23 -0400

    Do not break if a repository could not be found.
    [Finishes #91294918]

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,7 @@
 class RepositoriesController < ApplicationController
   def show
     render locals: { repository: current_user.repository(params[:owner], params[:repo_name]) }
+  rescue Octokit::NotFound
+    render locals: { repository: nil }
   end
 end
