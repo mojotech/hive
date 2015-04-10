da8b3ac8e92994eb5489a784a74b1f74b5703c50
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-04-10 14:58:53 -0400

    Create a file when a new branch is created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,9 @@ class User < ActiveRecord::Base
     repository = repository(owner, repo)
     default_branch_name = repository.default_branch
     root_sha = github_client.ref(repository.full_name, "heads/#{default_branch_name}").object.sha
-    github_client.create_ref(repository.full_name, "heads/#{name}", root_sha)
+    filename = filename_for_new_branch(name)
+    new_commit = create_new_file_commit(repository.full_name, root_sha, filename)
+    github_client.create_ref(repository.full_name, "heads/#{name}", new_commit.sha)
   end
 
   def branches(repository_full_name)
@@ -35,5 +37,40 @@ class User < ActiveRecord::Base
 
   def github_client
     @github_client ||= Octokit::Client.new(access_token: auth_token, auto_paginate: true)
+  end
+
+  def create_commit(repo, message, new_tree_sha, root_sha)
+    github_client.create_commit(repo, message, new_tree_sha, root_sha)
+  end
+
+  def filename_for_new_branch(branch_name)
+    "features/#{branch_name.underscore}.feature"
+  end
+
+  def create_blob(repo, content)
+    github_client.create_blob(repo, Base64.encode64(content), 'base64')
+  end
+
+  def file_tree_entry(filename, blob_sha)
+    [{
+      path: filename,
+      mode: '100644',
+      type: 'blob',
+      sha: blob_sha
+    }]
+  end
+
+  def create_file_tree(repo, base_tree_sha, filename, content)
+    blob_sha = create_blob(repo, content)
+    github_client.create_tree(
+      repo,
+      file_tree_entry(filename, blob_sha),
+      base_tree: base_tree_sha
+    )
+  end
+
+  def create_new_file_commit(repo, root_sha, filename, content = '')
+    new_tree = create_file_tree(repo, root_sha, filename, content)
+    create_commit(repo, "Added #{filename}", new_tree.sha, root_sha)
   end
 end
