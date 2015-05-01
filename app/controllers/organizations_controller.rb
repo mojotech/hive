8a548eefe42b28cf4f620ae45a1e515f96a8a26e
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-05-01 15:20:48 -0400

    Sort repositories in list by name.
    [Finishes #91295486]

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,6 +2,6 @@ class OrganizationsController < ApplicationController
   def show
     add_breadcrumb params[:login]
 
-    render locals: { repositories: current_user.repositories_for_organization(params[:login]) }
+    render locals: { repositories: current_user.repositories_for_organization(params[:login]).sort_by { |repo| repo[:name].downcase } }
   end
 end
