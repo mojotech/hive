f0a8b84369f7337af3f55e822e7ed9d4b99ce6f7
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-10-30 14:49:00 -0400

    Add repository_owner to apps table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023174108) do
+ActiveRecord::Schema.define(version: 20151030184705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151023174108) do
   create_table "apps", force: :cascade do |t|
     t.string "name"
     t.string "repository_name"
+    t.string "repository_owner"
   end
 
   create_table "apps_users", force: :cascade do |t|
