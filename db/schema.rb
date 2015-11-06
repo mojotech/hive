52ea1349fa072f08766f0a5dc879980ae31d6893
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-11-06 13:43:55 -0500

    Add points column to tickets.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030184705) do
+ActiveRecord::Schema.define(version: 20151106184149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151030184705) do
     t.integer  "lane_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "points"
   end
 
   add_index "tickets", ["lane_id"], name: "index_tickets_on_lane_id", using: :btree
