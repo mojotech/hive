34ad239335149e1a4506d70fa68393537e54a474
David Dufresne <daviddufresne@Dufresnes-MacBook-Pro.local>
2015-12-18 14:24:38 -0500

    Require spec doubles in cucumber.

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,7 @@
 
 require 'cucumber/rails'
 require 'dill/cucumber'
+require 'cucumber/rspec/doubles'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
