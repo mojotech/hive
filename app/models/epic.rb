08037e8ebcd65005b4ec66738cc8259c3bb88b31
David Dufresne <dufresne@mojotech.com>
2016-03-18 15:17:18 -0400

    Squash

--- app/models/epic.rb
+++ app/models/epic.rb
@@ -1,4 +1,5 @@
 class Epic < ActiveRecord::Base
   belongs_to :app
+  has_and_belongs_to_many :tickets
   validates :app, presence: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ActiveRecord::Base
   has_many :acceptance_criteria
   belongs_to :requester, class_name: 'User'
   belongs_to :owner, class_name: 'User'
+  has_and_belongs_to_many :epics
 
   validates :requester, presence: true
   validates :lane, presence: true
