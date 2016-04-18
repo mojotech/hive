e4a74670ce8f2ff98e916a9bb777559607388fda
Ryan Foster <ryan.foster@viget.com>
2016-04-18 17:57:06 -0600

    Refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < ApplicationController
+
   def create
-    auth = request.env['omniauth.auth']
-
-    user = User.find_by(github_user_id: auth['uid']) || create_user(auth)
-
     session[:user_id] = user.id
 
     redirect_to root_url, notice: 'Signed in!'
@@ -11,15 +8,30 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+
     redirect_to root_url, notice: 'Signed out!'
   end
 
   private
 
-  def create_user(auth)
-    User.create(github_user_id: auth['uid'],
-                nickname: auth['info']['nickname'],
-                email:  auth['info']['email'],
-                auth_token: auth['credentials']['token'])
+  def user
+    find_user || create_user
+  end
+
+  def find_user
+    User.find_by(github_user_id: auth_data['uid'])
+  end
+
+  def create_user
+    User.create(
+      github_user_id: auth_data['uid'],
+      nickname:       auth_data['info']['nickname'],
+      email:          auth_data['info']['email'],
+      auth_token:     auth_data['credentials']['token']
+    )
+  end
+
+  def auth_data
+    request.env['omniauth.auth']
   end
 end
