397037aa0adb97413df949274983552971e1b433
David Dufresne <dufresne@mojotech.com>
2016-05-06 13:46:44 -0400

    Use single quotes.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     user_session.create
-    flash[:notice] = "Signed in!"
+    flash[:notice] = 'Signed in!'
 
     redirect_to root_url
   end
 
   def destroy
     user_session.destroy
-    flash[:notice] = "Signed out!"
+    flash[:notice] = 'Signed out!'
 
     redirect_to root_url
   end
