6fd88264d372ba46babfd094da46e4280c3c624f
David Dufresne <dufresne@mojotech.com>
2016-05-20 13:22:29 -0400

    Add breadcrumbs to document page.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  before_action :add_breadcrumbs, only: [:show]
+
   def show
     sha = params[:id]
     app = current_app
@@ -14,5 +16,9 @@ class DocumentsController < ApplicationController
 
   def current_app
     @current_app ||= current_user.apps.find(params[:app_id])
+  end
+
+  private def add_breadcrumbs
+    add_breadcrumb current_app.name, app_path(current_app)
   end
 end
